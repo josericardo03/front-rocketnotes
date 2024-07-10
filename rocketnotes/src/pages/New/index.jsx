7e36ea8773939b6278c5f3dc8011e4cf513d84c7
josericardo03 <borges2001jose@gmail.com>
2024-07-10 12:59:12 -0400

    teste

---
+++ rocketnotes/src/pages/New/index.jsx
@@ -0,0 +1,31 @@
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { Textarea } from "../../components/Textarea";
+import { NoteItem } from "../../components/NoteItem";
+import { Container, Form } from "./styles";
+import { Section } from "../../components/Section";
+
+export function New() {
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="/">voltar</a>
+          </header>
+
+          <Input placeholder="Título" />
+          <Textarea placeholder="Observações" />
+
+          <Section title="links úteis">
+            <NoteItem value="https://rocketseat.com.br" />
+            <NoteItem isNew placeholder="Novo link" />
+          </Section>
+        </Form>
+      </main>
+    </Container>
+  );
+}
